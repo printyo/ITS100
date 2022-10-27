7c3ae7810b27423d97ae8150f6c64b5d3c7289b5
printyo <print.krittanut@gmail.com>
2022-10-27 10:37:47 +0700

    Update ex6.py

---
+++ Lab8 Dictionary and Set/ex6.py
@@ -0,0 +1,16 @@
+mydict = {}
+while True:
+    string = input("Input (DONE = exit): ")
+    if string == "DONE":
+        print("Output:")
+        for i in sorted(mydict.values(), reverse=True):
+            for j in mydict:
+                if mydict[j] == i:
+                    print(j,i)
+        break
+    key, val = string.split()
+    if key.isnumeric() and val.isnumeric():
+        if int(key) > 0 and int(val) > 0:
+                mydict[key] = val
+    else:
+        print("Invalid input")
