406fec0e19c079bc1d82f7c0ee6e6cd0c3194899
printyo <print.krittanut@gmail.com>
2022-11-10 13:46:08 +0700

    lab10 v3

--- Lab10 Numerical Computing/ex4.py
+++ Lab10 Numerical Computing/ex4.py
@@ -1,25 +1,29 @@
 import numpy as np
 
-data = np.loadtxt("Lab10 Numerical Computing/sales.tsv", delimiter='\t', dtype=float)
+data = np.loadtxt("Lab10 Numerical Computing/sales.tsv", delimiter='\t', dtype=float) #import file
 
-branch = data[0:,0]
-prodSale = data[0:,1:]
+branch = data[0:,0] #took first column of branch id
+prodSale = data[0:,1:] #took total product sales
 
-summ = np.sum(prodSale, axis =1)
-print(summ)
-indices = np.argsort(summ,kind="mergesort")
+summ = np.sum(prodSale, axis =1) #get sum of each row
+order = np.sort(summ) #sort into ascending order
 
-print(branch,summ, indices)
-n = 0
-m = 0
+n = len(order) - 1 #this code block reverses the array
+lis = []
+while n >=0:
+    lis.append(order[n])
+    n -= 1
+myArr = np.array(lis) #turn back to matrix why not idk
+
 myList = []
-for j in range(len(summ)):
-    for i in indices:
-        if i == n:
-            myList.append([branch[m],summ[m]])
-            n += 1
-        m +=1
-    m = 0
-    
-print(myList)
-#yung mai sedß
+for i in range(5):
+    myList.append([branch[i],summ[i]])
+myArray = np.array(myList) #join branch id with sum product sold to corresponding branch
+
+for j in range(5): #loop to find branch from max to min
+    for i in range(5):
+        if myArr[j] == myArray[i,1]:
+            print(myArray[i])
+
+
+
